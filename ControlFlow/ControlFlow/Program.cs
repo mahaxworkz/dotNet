580873f165e10727834636a30521860272f6dfa3
unknown <mahalakshmi.xworkz@gmail.com>
2024-03-16 21:05:47 +0530

    cs code

---
+++ ControlFlow/ControlFlow/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace ControlFlow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter your name : ");
+            String  name= Console.ReadLine();
+           
+            char letter = 'z';
+             for(int i = 0; i < name.Length; i++)
+            {
+                int count = 0;
+                for(int j=i; j < name.Length; j++)
+                {
+                    if (name[i].Equals(name[j]) )
+                    {
+                        
+                        count++;
+                        
+                    }
+                    
+                }
+                Console.WriteLine( "count of "+name[i]+" : "+count);
+            }
+        }
+    }
+}
---
+++ TwoDimArray/TwoDimArray/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace TwoDimArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] months = { "jan", "feb", "march" };
+            int[,] sales = new int[3, 3];
+            for(int i = 0; i < sales.GetLength(0); i++)
+            {
+                Console.WriteLine($"Enter the sales for salesman- {i + 1}");
+                for(int j=0;j<sales.GetLength(1); j++)
+                {
+                    Console.WriteLine($"Enter the sales for {months[j]} ");
+                    sales[i,j]= int.Parse(Console.ReadLine());
+                }
+            }
+
+            for (int i = 0; i < sales.GetLength(0); i++)
+            {
+                int tot = 0;
+                Console.Write($"salesman- {i + 1}\t");
+                for (int j = 0; j < sales.GetLength(1); j++)
+                {
+                    Console.Write($"{sales[i,j]}\t ");
+                    
+                    tot += sales[i,j];
+                }
+                Console.WriteLine(tot);
+            }
+        }
+    }
+}
