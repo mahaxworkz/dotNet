f6efe955792d26f78e7008e1bf42c06cc0e30b78
unknown <mahalakshmi.xworkz@gmail.com>
2024-03-16 21:00:30 +0530

    c#code

---
+++ FirstProgram/FirstProgram/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace  FirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int p,n,r,si;
+            Console.Write("Enter  the value of p");
+            p=int.Parse(Console.ReadLine());
+            Console.Write("Enter  the value of n");
+            n = int.Parse(Console.ReadLine());
+            Console.Write("Enter  the value of r");
+            r = int.Parse(Console.ReadLine());
+            si = (p * n * r) / 100;
+            Console.WriteLine($"value of si is {si}.");
+
+
+
+        }
+    }
+}
