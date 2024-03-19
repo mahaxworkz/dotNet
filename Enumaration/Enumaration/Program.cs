54bcc044adb8ca88e64a9f4484e630db9169d5c8
unknown <mahalakshmi.xworkz@gmail.com>
2024-03-19 19:00:00 +0530

    enum

---
+++ Enumaration/Enumaration/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Enumaration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            direction d= direction.east;
+            Console.WriteLine((int)d);
+        }
+
+        public enum direction { north=2,south,east,west}
+    }
+}
