04e0d00a6916448d85642f1c4b30df5059da60ff
unknown <mahalakshmi.xworkz@gmail.com>
2024-03-19 18:44:18 +0530

    excetion

---
+++ ExceptionHandling/ExceptionHandling/Exception01.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+    internal class Exception01 : Exception
+    {
+        String msg;
+         public Exception01(String msg) : base(msg) { 
+           this. msg=msg;
+        
+        }
+        public override string ToString()
+        {
+            return msg;
+        }
+
+
+ 
+    }
+}
---
+++ ExceptionHandling/ExceptionHandling/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace ExceptionHandling
+{
+     
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+
+            Console.Write("Enter your name : ");
+            name = Console.ReadLine();
+            try
+            {
+                CheckError(name);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(String.Concat(e.StackTrace, e.Message));
+            }
+             
+           
+            
+        }
+        public static void  CheckError(String name)
+        {
+            if (name.Length < 3)
+            {
+                throw new Exception01("name must have more than 3 charachters");
+            }
+            else
+            {
+                Console.WriteLine(name);
+            }
+
+        }
+    }
+}
